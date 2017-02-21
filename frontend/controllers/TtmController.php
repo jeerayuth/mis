4a643ae5dc8df1ae80097c96393c7505e11acbe1
user <user@ADMIN-CENTER>
2017-02-21 09:37:33 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/controllers/TtmController.php
+++ frontend/controllers/TtmController.php
@@ -141,5 +141,187 @@ class TtmController extends CommonController {
         ]);
     }
    
+    public function actionReport4($datestart, $dateend, $details) {
+                // save log
+        $this->SaveLog($this->dep_controller, 'report4', $this->getSession());
+
+        $report_name = "รายงานมูลค่าการสั่งใช้ยาแพทย์แผนไทยทั้งหมด";
+        $report_name_graph = "รายงาน 10 อันดับมูลค่าการสั่งใช้ยาแพทย์แผนไทย";
+         
+        $sql = "SELECT
+                    o.icode,d.name as drug_name,sum(o.qty) as sum_qty,
+                     sum(o.sum_price) as sum_price,
+                     count(distinct(o.hn)) as count_hn,
+                     count(o.hn) as count_useage
+                FROM opitemrece o
+                LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                WHERE 
+                    o.vstdate BETWEEN $datestart and $dateend  AND 
+                    d.did like '4%'
+                GROUP BY o.icode
+                ORDER BY sum_price DESC ";
+        
+        $sql_graph = "SELECT
+                    o.icode,d.name as drug_name,sum(o.qty) as sum_qty,
+                     sum(o.sum_price) as sum_price
+                FROM opitemrece o
+                LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                WHERE 
+                    o.vstdate BETWEEN $datestart and $dateend  AND 
+                    d.did like '4%'
+                GROUP BY o.icode
+                ORDER BY sum_price DESC
+                LIMIT 10 ";
+        
+
+        try {
+            $rawData = \yii::$app->db->createCommand($sql)->queryAll();
+            $rawData_graph = \yii::$app->db->createCommand($sql_graph)->queryAll();
+        } catch (\yii\db\Exception $e) {
+            throw new \yii\web\ConflictHttpException('sql error');
+        }
+
+        $dataProvider = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData,
+            'pagination' => FALSE,
+        ]);
+        
+        $dataProvider_graph = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData_graph,
+            'pagination' => FALSE,
+        ]);
+
+        return $this->render('report4', [
+                    'dataProvider' => $dataProvider,
+                    'dataProvider_graph' => $dataProvider_graph,
+                    'rawData' => $rawData,
+                    'rawData_graph' => $rawData_graph,
+                    'report_name' => $report_name,
+                    'report_name_graph' => $report_name_graph,
+                    'details' => $details,
+        ]);
+    }
+    
+    
+    public function actionReport5($datestart, $dateend, $details) {
+                // save log
+        $this->SaveLog($this->dep_controller, 'report5', $this->getSession());
+
+        $report_name = "รายงานมูลค่าการสั่งใช้ยาแพทย์แผนไทยทั้งหมดแยกตามแพทย์ผู้สั่ง";
+        $report_name_graph = "รายงาน 10 อันดับมูลค่าการสั่งใช้ยาแพทย์แผนไทยแยกตามแพทย์ผู้สั่ง";      
+        $sql = "SELECT
+                       doc.name as doctor_name,sum(o.qty) as sum_qty,
+                       sum(o.sum_price) as sum_price,
+                       count(distinct(o.hn)) as count_hn,
+                       count(distinct(o.vn)) as count_visit ,
+                       count(o.hn) as count_useage
+                 FROM opitemrece o
+                 LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                 LEFT OUTER JOIN doctor doc ON doc.code = o.doctor
+                 WHERE o.vstdate BETWEEN $datestart and $dateend  AND d.did like '4%'
+                 GROUP BY o.doctor
+                 ORDER BY sum_price DESC ";
+        
+        $sql_graph = "SELECT
+                        doc.name as doctor_name,sum(o.qty) as sum_qty,
+                        sum(o.sum_price) as sum_price
+                 FROM opitemrece o
+                 LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                 LEFT OUTER JOIN doctor doc ON doc.code = o.doctor
+                 WHERE o.vstdate BETWEEN $datestart and $dateend  AND d.did like '4%'
+                 GROUP BY o.doctor
+                 ORDER BY sum_price DESC
+                 LIMIT 10 ";
+        
+        try {
+            $rawData = \yii::$app->db->createCommand($sql)->queryAll();
+            $rawData_graph = \yii::$app->db->createCommand($sql_graph)->queryAll();
+        } catch (\yii\db\Exception $e) {
+            throw new \yii\web\ConflictHttpException('sql error');
+        }
+
+        $dataProvider = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData,
+            'pagination' => FALSE,
+        ]);
+        
+        $dataProvider_graph = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData_graph,
+            'pagination' => FALSE,
+        ]);
+
+        return $this->render('report5', [
+                    'dataProvider' => $dataProvider,
+                    'dataProvider_graph' => $dataProvider_graph,
+                    'rawData' => $rawData,
+                    'rawData_graph' => $rawData_graph,
+                    'report_name' => $report_name,
+                    'report_name_graph' => $report_name_graph,
+                    'details' => $details,
+        ]);
+    }
+    
+    
+    public function actionReport6($datestart, $dateend, $details) {
+                // save log
+        $this->SaveLog($this->dep_controller, 'report6', $this->getSession());
+
+        $report_name = "รายงานมูลค่าการสั่งใช้ยาแพทย์แผนไทยทั้งหมดแยกตามสิทธิ์การรักษา";
+        $report_name_graph = "รายงาน 10 อันดับมูลค่าการสั่งใช้ยาแพทย์แผนไทยแยกตามสิทธิ์การรักษา ";      
+        $sql = "SELECT
+                     o.pttype,pt.name as pt_type_name,
+                     sum(o.qty) as sum_qty,
+                     sum(o.sum_price) as sum_price  ,
+                     count(distinct(o.hn)) as count_hn,
+                     count(o.hn) as count_useage
+                 FROM opitemrece o
+                 LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                 LEFT OUTER JOIN pttype pt ON pt.pttype = o.pttype
+                 WHERE o.vstdate BETWEEN $datestart and $dateend  AND d.did like '4%'
+                 GROUP BY o.pttype 
+                 ORDER BY sum_price DESC ";
+        
+        $sql_graph = "SELECT
+                     o.pttype,pt.name as pt_type_name,
+                     sum(o.qty) as sum_qty,
+                     sum(o.sum_price) as sum_price 
+                 FROM opitemrece o
+                 LEFT OUTER JOIN drugitems d ON d.icode = o.icode
+                 LEFT OUTER JOIN pttype pt ON pt.pttype = o.pttype
+                 WHERE o.vstdate BETWEEN $datestart and $dateend  AND d.did like '4%'
+                 GROUP BY o.pttype 
+                 ORDER BY sum_price DESC 
+                 LIMIT 10 ";
+        
+        try {
+            $rawData = \yii::$app->db->createCommand($sql)->queryAll();
+            $rawData_graph = \yii::$app->db->createCommand($sql_graph)->queryAll();
+        } catch (\yii\db\Exception $e) {
+            throw new \yii\web\ConflictHttpException('sql error');
+        }
+
+        $dataProvider = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData,
+            'pagination' => FALSE,
+        ]);
+        
+        $dataProvider_graph = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData_graph,
+            'pagination' => FALSE,
+        ]);
+    
+        return $this->render('report6', [
+                    'dataProvider' => $dataProvider,
+                    'dataProvider_graph' => $dataProvider_graph,
+                    'rawData' => $rawData,
+                    'rawData_graph' => $rawData_graph,
+                    'report_name' => $report_name,
+                    'report_name_graph' => $report_name_graph,
+                    'details' => $details,
+        ]); 
+    }
+
+
+    
 
 }
---
+++ frontend/views/ttm/report6.php
@@ -0,0 +1,143 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use kartik\grid\GridView;
+use yii\helpers\Html;
+use miloschuman\highcharts\Highcharts;
+
+$this->title = $report_name;
+//$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+<div style="display:none">
+    <?php
+    echo Highcharts::widget([
+        'scripts' => [
+            'highcharts-more', // enables supplementary chart types (gauge, arearange, columnrange, etc.)
+            'themes/grid'        // applies global 'grid' theme to all charts
+        ],
+    ]);
+    ?>  
+</div>
+
+
+<div id="chart"></div>
+
+
+
+<?php
+//เตรียมชุดข้อมูลไปใส่ให้กราฟ แกน x,y
+$data = [];
+for ($i = 0; $i < count($rawData_graph); $i++) {
+    $data[] = [
+        'name' => $rawData_graph[$i]['pt_type_name'],
+        'y' => $rawData_graph[$i]['sum_price'] * 1,
+    ];
+}
+
+$js_data = json_encode($data);
+
+
+
+// chart
+$this->registerJs(" 
+    $(function () {
+    $('#chart').highcharts({
+        chart: {
+            type: 'column'
+        },
+         credits: {
+            enabled: false
+        },
+        title: {
+            text: '$report_name_graph'
+        },
+        
+         yAxis: {
+            min: 0,
+            title: {
+                text: 'มูลค่าการสั่งใช้ยาแพทย์แผนไทย(บาท)'
+            },
+            
+        },
+         xAxis: {
+            type: 'category'
+        },
+       legend: {
+            enabled: true
+        },
+
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        series: [{
+                name: 'รายการสิทธิ์การรักษา',
+                colorByPoint: true,
+                data:$js_data
+        }]
+     });
+    });
+");
+// จบ chart
+?>
+
+
+<br/>
+
+
+<?php
+
+echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'panel' => [
+        'heading' => $report_name,
+        'before' => $details,
+        'type' => 'primary',
+        'after' => 'ประมวลผล ณ วันที่ ' . date('Y-m-d H:i:s')
+    ],
+    'export' => [
+        'fontAwesome' => true,
+        'showConfirmAlert' => false,
+        'target' => GridView::TARGET_BLANK
+    ],
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'], 
+        
+        [
+            'attribute' => 'pt_type_name',
+            'header' => 'ชื่อสิทธิ์การรักษา'
+        ],
+        [
+            'attribute' => 'count_hn',
+            'header' => 'จำนวนคนไข้ที่สั่งใช้(คน)'
+        ],
+        /*
+        [
+            'attribute' => 'count_useage',
+            'header' => 'จำนวนครั้งที่สั่งใช้ยา'
+        ],
+        [
+            'attribute' => 'sum_qty',
+            'header' => 'ปริมาณสั่งใช้'
+        ],
+         
+         */
+        [
+            'attribute' => 'sum_price',
+            'header' => 'มูลค่าการใช้ยาแพทย์แผนไทย(บาท)'
+        ],
+        
+       
+         
+        
+    ]
+])
+?>
+
